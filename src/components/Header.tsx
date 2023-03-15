bf7e7bd5f4801b212f18e453d54b4e8db77cf469
0xnotnull <0xnotevennull@gmail.com>
2023-03-15 18:10:16 +0000

    Move IVX Logo Down Slightly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,15 @@ import CustomConnectButton from './CustomConnectButton'
 const Header: React.FC = () => {
   return (
     <div className="z-50 flex h-16 w-full flex-none items-center justify-between gap-2.5 bg-headerDark pr-8 pl-16  ">
-      <Image
-        alt="logo"
-        src="/IVX_Gradient.svg"
-        width={94.12}
-        height={20}
-        quality={100}
-      />
+      <div className="flex pt-3">
+        <Image
+          alt="logo"
+          src="/IVX_Gradient.svg"
+          width={94.12}
+          height={20}
+          quality={100}
+        />
+      </div>
       <div className="flex gap-8">
         <CustomConnectButton />
         <ThemeSwitch />
