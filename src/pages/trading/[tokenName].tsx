615943e22077e78e7ff578e2d955fccf5e39dd9c
0xnotnull <0xnotevennull@gmail.com>
2023-03-15 18:05:18 +0000

    Expand Chart to Fill Space

--- src/pages/trading/[tokenName].tsx
+++ src/pages/trading/[tokenName].tsx
@@ -9,7 +9,7 @@ type TradingPageProps = {
 const MarketTradingPage: React.FC<TradingPageProps> = ({ tokenName }) => {
   return (
     <div className="flex h-full">
-      <div className="w-full shrink overflow-y-hidden px-10 pt-4 ">
+      <div className="w-full shrink overflow-y-hidden pl-4 pt-4 ">
         <ChartContainer />
         <TokenPositionsPanel />
       </div>
