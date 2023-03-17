bc5da6b55a906ba95816d90c9caa38b98f328795
0xnotnull <0xnotevennull@gmail.com>
2023-03-17 10:17:40 +0000

    open/closed positions

--- src/components/Positions/Token/TokenPositionsPanel.tsx
+++ src/components/Positions/Token/TokenPositionsPanel.tsx
@@ -1,19 +1,28 @@
-import tokenIcon from '@/hooks/tokenIcon'
-import { useRouter } from 'next/router'
+import Tabs from '@/components/shared/Tabs'
+import { TabType } from '@/types/next'
 import PositionsCompactTable from './PositionsCompactTable'
 import TokenClosedPositionsPanel from './TokenClosedPositionsPanel'
 
 const TokenPositionsPanel = () => {
-  const router = useRouter()
-  const tokenSymbol = router.asPath.split('/').pop() ?? ''
+  const tableTabs: TabType[] = [
+    {
+      label: 'Open',
+      action: () => {},
+    },
+    {
+      label: 'Closed',
+      action: () => {},
+    },
+  ]
+
   return (
     <div className="flex w-full items-start gap-8 overflow-y-auto pb-14">
-      <div className="flex w-full flex-col items-start rounded-lg bg-gray-400 py-8 px-6 dark:bg-sectionsBGDark">
-        <div className="flex items-center gap-4 px-6 pb-4 text-lg font-semibold">
-          Positions
+      <div className="flex w-full flex-col items-start rounded-lg  py-8 px-6 ">
+        <div className="flex items-center px-6 pb-4 ">
+          <Tabs tabList={tableTabs} size="md" style="monochromatic" />
         </div>
         <PositionsCompactTable statusToShow="Open" />
-        <TokenClosedPositionsPanel />
+        {/* <TokenClosedPositionsPanel /> */}
       </div>
     </div>
   )
