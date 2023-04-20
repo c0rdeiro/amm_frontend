f58ef1c12c7424ed411ea1d8f7af7a89fd3b25b8
0xnotnull <0xnotevennull@gmail.com>
2023-04-20 10:37:03 +0100

    responsiveness

--- src/pages/trading/[tokenName].tsx
+++ src/pages/trading/[tokenName].tsx
@@ -8,7 +8,7 @@ type TradingPageProps = {
 
 const MarketTradingPage: React.FC<TradingPageProps> = ({ tokenName }) => {
   return (
-    <div className="flex h-full w-full justify-between text-white">
+    <div className="flex h-full w-full flex-col justify-between text-white xl:flex-row">
       <div className="w-full shrink pl-4 pt-4">
         {/* <Chart />
         <TokenPositionsPanel /> */}
