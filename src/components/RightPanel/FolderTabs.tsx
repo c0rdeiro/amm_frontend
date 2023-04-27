78e490faeaae69aaa085c4494d580b2e6044bdc2
0xnotnull <0xnotevennull@gmail.com>
2023-04-27 17:06:17 +0100

    tabs fixed

--- src/components/RightPanel/FolderTabs.tsx
+++ src/components/RightPanel/FolderTabs.tsx
@@ -28,27 +28,45 @@ const FolderTabs: React.FC<FolderTabsProps> = ({
       <Tab.List className="flex w-full ">
         {tabList.map((item) => (
           <Tab as={Fragment} key={item.key}>
-            <button
-              disabled={item.isDisabled}
-              onClick={item.action}
-              className={clsx(
-                'w-full border border-gray-500 py-2 focus-visible:outline-none',
-                'ui-selected:rounded-t-lg ui-selected:border-b-0  ui-selected:bg-gray-600',
-                'ui-not-selected:mt-1 ',
-                {
-                  'ui-not-selected:ml-2 ui-not-selected:rounded-tl-lg ui-not-selected:border-r-0':
-                    item.key === 0,
-                  'ui-not-selected:mr-2 ui-not-selected:rounded-tr-lg ui-not-selected:border-l-0 ':
-                    item.key === 1,
-                }
-              )}
-            >
-              <span className="flex w-full items-center justify-center gap-2">
-                {item.leftIcon}
-                {item.label}
-                {item.rightIcon}
-              </span>
-            </button>
+            {({ selected }) => (
+              <div
+                onClick={item.action}
+                className={clsx(
+                  'relative flex w-full cursor-pointer items-center justify-center border border-gray-500 py-2 focus-visible:outline-none',
+                  'ui-selected:rounded-t-lg ui-selected:border-b-0  ui-selected:bg-gray-600',
+                  'ui-not-selected:mt-1 ',
+                  {
+                    'ui-not-selected:ml-2 ui-not-selected:rounded-tl-lg ui-not-selected:border-r-0':
+                      item.key === 0,
+                    'ui-not-selected:mr-2 ui-not-selected:rounded-tr-lg ui-not-selected:border-l-0 ':
+                      item.key === 1,
+                  }
+                )}
+              >
+                {selected && item.key === 1 && (
+                  <div
+                    className={
+                      'absolute -left-2 bottom-0 z-10 h-full w-4 -skew-x-[20deg] rounded-tl border-t border-l border-gray-500 bg-gray-600'
+                    }
+                  />
+                )}
+
+                <button disabled={item.isDisabled}>
+                  <span className="flex w-full items-center justify-center gap-2">
+                    {item.leftIcon}
+                    {item.label}
+                    {item.rightIcon}
+                  </span>
+                </button>
+                {selected && item.key === 0 && (
+                  <div
+                    className={
+                      'absolute -right-2 bottom-0 z-10 h-full w-4 skew-x-[20deg] rounded-tr border-t border-r border-gray-500 bg-gray-600'
+                    }
+                  />
+                )}
+              </div>
+            )}
           </Tab>
         ))}
       </Tab.List>
